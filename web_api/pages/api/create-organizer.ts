7442228e8e3ee4ef2a8b6c6608cc493d0e72151e
wfchiang <weifan.wf@gmail.com>
2024-09-09 14:38:43 -0400

    add the very first version of web-api

---
+++ web_api/pages/api/create-organizer.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req :NextApiRequest, res :NextApiResponse) {
+  // Accept POST only 
+  if (req.method !== 'POST') {
+    res.status(405).json({ message: 'Method not allowed' });
+    return;
+  }
+
+  // Check the payload 
+  const { name, email } = req.body; 
+
+  res.status(200).json({ message: `Got email ${email}` });
+}
