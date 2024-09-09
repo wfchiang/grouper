59d201a0cb25401954f8021fb9ac9e4f8da8bbee
wfchiang <weifan.wf@gmail.com>
2024-09-09 16:44:19 -0400

    try to fail the auth

--- web_api/firebase.tsx
+++ web_api/firebase.tsx
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "this-should-fail", // process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
