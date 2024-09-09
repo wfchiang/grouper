744cc660d99dcecc125edd902ae8ccc02689353d
wfchiang <weifan.wf@gmail.com>
2024-09-09 14:43:19 -0400

    try to fix the linter issue of the first deployment

--- web_api/pages/api/create-organizer.ts
+++ web_api/pages/api/create-organizer.ts
@@ -10,5 +10,5 @@ export default function handler(req :NextApiRequest, res :NextApiResponse) {
   // Check the payload 
   const { name, email } = req.body; 
 
-  res.status(200).json({ message: `Got email ${email}` });
+  res.status(200).json({ message: `Got email ${email} of ${name}` });
 }
